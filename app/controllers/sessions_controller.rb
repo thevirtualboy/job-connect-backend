dc9f7cc9f511303cfe3154c0ff0a3e9085ca9a92
Peter Fontana <dude0508@gmail.com>
2021-12-07 20:20:13 -0500

    minor changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(id: params[:id])
+        user = User.find_by(email: params[:email])
         if user&.authenticate(params[:password])
           session[:user_id] = user.id
           render json: user, status: :created
