1c19ace9992433040bc93b0ee8c04a6c119a4dde
bricejokung <bjokung@gmail.com>
2021-12-07 20:17:03 -0500

    front end day 2 done

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -7,6 +7,8 @@ import Login_SignupPage from './Login_SignupPage';
 function App() {
 
   const [users, setUsers] = useState([])
+  const [login, setLogin] = useState(false)
+  const [uselog, setUselog] = useState("")
 
   useEffect(()=>{
     fetch('http://localhost:3000/users')
@@ -15,16 +17,23 @@ function App() {
       console.log(users)})
   }, [])
 
+  
+
   function onLogin(user){
-   
+   setLogin(true)
+   setUselog(user.name)
 
     
 
   }
+
+
   return (
     <div >
       <Header />
-      <Login_SignupPage/>
+      <Login_SignupPage onLogin={onLogin}/>
+      {login? <p>{uselog}</p>:<p>not Logged in</p>}
+
 
     </div>
   );
--- client/src/Components/Login_SignupPage.js
+++ client/src/Components/Login_SignupPage.js
@@ -1,10 +1,24 @@
 import {useState} from 'react'
 
-function Login_SignupPage () {
+function Login_SignupPage ({onLogin}) {
 
-    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-    function handleSubmit(){
+    function handleSubmit(e){
+        e.preventDefault()
+        
+        fetch('/login', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({email, password}),
+
+        })
+        .then(response => response.json())
+        .then(data => {onLogin(data)
+        console.log(data)})
         
         
     }
@@ -12,14 +26,14 @@ function Login_SignupPage () {
     
     <>
     <h1> Login Form </h1>
-    <form> 
+    <form onSubmit = {handleSubmit}>  
     <label>Email <br/>
-    <input type="text" placeholder= "1234type@gmail.com"/> 
+    <input type="text" placeholder= "1234type@gmail.com" onChange = {(e)=>setEmail(e.target.value)}/> 
     </label> <br/>  
     <label>Password <br/>
-    <input type="text" placeholder= "abcde123!"/> 
+    <input type="text" placeholder= "abcde123!"onChange = {(e)=>setPassword(e.target.value)} /> 
     </label> 
-    <button>Submit</button>
+    <button type="submit">Submit</button>
     </form>
 
     </>
