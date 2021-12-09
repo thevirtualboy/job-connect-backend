6fe443b88c60f84d94b30a44046e59eb5b536fe2
aurionhoward <aurionhoward@gmail.com>
2021-12-09 13:08:44 -0500

    added styling

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,4 +1,28 @@
 import { Link } from 'react-router-dom'
+import logo  from '../logo.JPG'
+
+
+const headStyle = {
+    backgroundColor: "white"
+}
+const logoStyle = {
+    height: `150px`
+
+}
+
+const btnStyle ={
+    float: "right",
+    marginTop: "60px",
+    color: "white",
+    backgroundColor: "#0A8FF1",
+    border: "1.5px solid black",
+    borderRadius: "10px",
+    fontSize:"20px",
+    padding: "4px",
+    fontFamily: "Genos, sans-serif"
+
+}
+
 function Header ({onLogout, login}) {
     function handleLogout() {
         fetch("/logout", {
@@ -6,16 +30,16 @@ function Header ({onLogout, login}) {
         }).then(() => onLogout());
     }
     return (
-        <>
-        <h1> Job Connect </h1>
+        <div style={headStyle}>
+         <img style={logoStyle} src={logo} />
         {login ?
-            <button onClick={handleLogout}>Logout </button>
+            <button style={btnStyle} onClick={handleLogout}>Logout </button>
             :
             <Link to="/login">
-                <button> Login/Signup </button>
+                <button style={btnStyle}> Login/Signup </button>
             </Link>
         }
-        </>
+        </div>
     )
 }
 export default Header
--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,25 +1,43 @@
 import { NavLink } from "react-router-dom"
 
 
+const linkStyle ={
+    textDecoration:"none",
+    color: "#535B61",
+    padding: "3px 6px 3px 6px",
+    borderRadius: "5px",
+    backgroundColor: "white"
+}
+
+const navStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingTop: "20px",
+    fontFamily: "Genos, sans-serif",
+    fontSize: "25px"
+    
+}
+
 function NavBar () {
     return (
-        <>
-            <NavLink 
+        <div style={navStyle}>
+            <NavLink className="nav" style={linkStyle}
                 to="/"    
             >
                 About Us
             </NavLink>
-            <NavLink
+            <NavLink className="nav" style={linkStyle}
                 to="/jobs"
             >
                 Job List
             </NavLink>
-            <NavLink
+            <NavLink className="nav" style={linkStyle}
                 to="/myjobs"
             >
                 My Jobs
             </NavLink>
-        </>
+        </div>
     )
 }
 export default NavBar
