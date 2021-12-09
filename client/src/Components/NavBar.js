a1dcb23e3009684a1ee86c9dcb70fd7a5eb1ca0e
Peter Fontana <dude0508@gmail.com>
2021-12-09 13:59:33 -0500

    added navbar hover style

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,38 +1,85 @@
+import { useState } from "react"
 import { NavLink } from "react-router-dom"
 
-
-const linkStyle ={
-    textDecoration:"none",
-    color: "#535B61",
-    padding: "3px 6px 3px 6px",
-    borderRadius: "5px",
-    backgroundColor: "white"
-}
-
-const navStyle = {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    paddingTop: "20px",
-    fontFamily: "Genos, sans-serif",
-    fontSize: "25px"
-    
-}
-
 function NavBar () {
+    const [hover1, setHover1] = useState(false)
+    const [hover2, setHover2] = useState(false)
+    const [hover3, setHover3] = useState(false)
+
+    let link1Style ={
+        textDecoration:"none",
+        color: "#535B61",
+        padding: "3px 6px 3px 6px",
+        borderRadius: "5px",
+        backgroundColor: "white"
+    }
+
+    let link2Style ={
+        textDecoration:"none",
+        color: "#535B61",
+        padding: "3px 6px 3px 6px",
+        borderRadius: "5px",
+        backgroundColor: "white"
+    }
+
+    let link3Style ={
+        textDecoration:"none",
+        color: "#535B61",
+        padding: "3px 6px 3px 6px",
+        borderRadius: "5px",
+        backgroundColor: "white"
+    }
+
+    if (hover1 === true) {
+        link1Style.color = "#0A8FF1"
+    } else {
+        link1Style.color = "#535B61"
+    }
+
+    if (hover2 === true) {
+        link2Style.color = "#0A8FF1"
+    } else {
+        link2Style.color = "#535B61"
+    }
+
+    if (hover3 === true) {
+        link3Style.color = "#0A8FF1"
+    } else {
+        link3Style.color = "#535B61"
+    }
+    
+    const navStyle = {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        paddingTop: "20px",
+        fontFamily: "Genos, sans-serif",
+        fontSize: "25px"
+        
+    }
+
     return (
         <div style={navStyle}>
-            <NavLink className="nav" style={linkStyle}
-                to="/"    
+            <NavLink 
+                style={link1Style}
+                onMouseOver={() => setHover1(true)}
+                onMouseOut={() => setHover1(false)}
+                to="/"
             >
                 About Us
             </NavLink>
-            <NavLink className="nav" style={linkStyle}
+            <NavLink
+                style={link2Style}
+                onMouseOver={() => setHover2(true)}
+                onMouseOut={() => setHover2(false)}
                 to="/jobs"
             >
                 Job List
             </NavLink>
-            <NavLink className="nav" style={linkStyle}
+            <NavLink 
+                style={link3Style}
+                onMouseOver={() => setHover3(true)}
+                onMouseOut={() => setHover3(false)}
                 to="/myjobs"
             >
                 My Jobs
